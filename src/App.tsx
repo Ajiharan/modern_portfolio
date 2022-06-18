553ceab3d3b64d0391e9b22d3ecdeed066f033e6
Ajiharan <baskaranajiharan1243@gmail.com>
2022-06-18 11:57:03 +0530

    animation added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import GlobalStyle from "styles/global";
 import Header from "components/header/Header";
+import Main from "components/main/Main";
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle></GlobalStyle>
       <Header />
+      <Main />
     </React.Fragment>
   );
 }
